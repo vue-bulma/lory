e469b9b5322e9f5e3ee401c14092dcb1dbfc543e
Fangdun Cai <cfddream@gmail.com>
2016-10-25 17:09:12 +0800

    init

---
+++ Lory.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="slider js_slider">
+    <div class="frame js_frame">
+      <ul class="slides js_slides">
+        <slot name="slide"></slot>
+      </ul>
+    </div>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<script>
+import { lory } from 'lory.js'
+
+export default {
+
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data () {
+    return {
+      slider: null
+    }
+  },
+
+  mounted () {
+    this.slider = lory(this.$el, this.options)
+  },
+
+  beforeDestroy () {
+    this.slider.destroy()
+  }
+
+}
+</script>
+
+<style>
+</style>
