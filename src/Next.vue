0a721fd4cd390048a40874b2e6c74a79be089eae
Fangdun Cai <cfddream@gmail.com>
2016-10-27 03:40:16 +0800

    Bump v0.0.4

--- src/Next.vue
+++ src/Next.vue
@@ -1,5 +1,20 @@
 <template>
   <span class="js_next next">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5"><g><path fill="#2E435A" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"></path></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 501.5 501.5"><g><path :fill="color" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"></path></g></svg>
   </span>
 </template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 50
+    },
+    color: {
+      type: String,
+      default: '#2E435A'
+    }
+  }
+}
+</script>
--- src/Prev.vue
+++ src/Prev.vue
@@ -1,5 +1,20 @@
 <template>
   <span class="js_prev prev">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5"><g><path fill="#2E435A" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"></path></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 501.5 501.5"><g><path :fill="color" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"></path></g></svg>
   </span>
 </template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 50
+    },
+    color: {
+      type: String,
+      default: '#2E435A'
+    }
+  }
+}
+</script>
